6ffe2c80e9ba909e973305edede94d6b47874c19
ciazhar <daisetsunakama@gmail.com>
2017-08-28 11:12:23 +0700

    add callback

--- src/main/kotlin/vertx/MainVerticle.kt
+++ src/main/kotlin/vertx/MainVerticle.kt
@@ -1,15 +1,28 @@
 package vertx
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
 
 class MainVerticle : AbstractVerticle() {
 
-  override fun start() {
-    // your code goes here...
-    vertx.createHttpServer().requestHandler({ req ->
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!")
-    }).listen(8080)
+  override fun start(future: Future<Void>) {
+    vertx
+      .createHttpServer()
+      .requestHandler({
+          req -> req
+              .response()
+              .putHeader("content-type", "text/plain")
+              .end("Hello from Vert.x!")
+      })
+      .listen(8080,{
+          result ->
+          if(result.succeeded()){
+              future.complete()
+          }
+          else{
+              future.fail(result.cause())
+          }
+      })
+
   }
 }
